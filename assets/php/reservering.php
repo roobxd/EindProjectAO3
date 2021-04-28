9b877563020b6b585bcb2b44d42a8af9bf35c6e3
roobxd <dabgamernl@gmail.com>
2021-04-28 12:08:17 +0200

    ayoo the pizza done!!

---
+++ assets/php/reservering.php
@@ -0,0 +1,56 @@
+<?php
+
+    require("db/database_connectie.php");
+
+    class Reservering {
+        public $plaatsnummer;
+        public $voornaam;
+        public $tussenvoegsel;
+        public $achternaam;
+        public $begin_datum;
+        public $eind_datum;
+        public $kinderen4_12;
+        public $huisdier;
+        public $elektriciteit;
+        public $douche;
+        public $wasmachine;
+        public $wasdroger;
+        public $caravan_klein;
+        public $caravan_groot;
+        public $tent_klein;
+        public $tent_groot;
+        public $auto;
+
+        function __construct(Array $prop=array()){
+            foreach($prop as $key => $value){
+                $this->{$key} = $value;
+            }
+        }
+
+
+        function return_small_result(){
+            $result = array(
+                $this->plaatsnummer,
+                $this->voornaam,
+                $this->tussenvoegsel,
+                $this->achternaam,
+                $this->begin_datum,
+                $this->eind_datum,
+            );
+            return $result;
+        } 
+    }
+
+
+    function returnReserveringen(){
+        $connection = OpenConnection();
+        $result = $connection->query("SELECT `plaatsnummer`, `klanten`.`voornaam`, `klanten`.`tussenvoegsel`, `klanten`.`achternaam`, `begin_datum`, `eind_datum`, `kinderen4_12`, `huisdier`, `elektriciteit`, `douche`, `wasmachine`, `wasdroger`, `caravan_klein`, `caravan_groot`, `tent_klein`, `tent_groot`, `auto` FROM `reserveringen`, `klanten`  WHERE `klanten`.`klant_id` = `reserveringen`.`klant_id`");
+        $reservering_array = array();
+        while ( $arr = $result->fetch_assoc() ){
+            array_push($reservering_array, new Reservering($arr)); 
+        }
+        CloseConnection($connection);
+        return $reservering_array;
+    }
+
+?>
--- dashboard/reserveringen.php
+++ dashboard/reserveringen.php
@@ -1,15 +1,21 @@
 <?php
     require("../assets/template/dashboard_template.html");
+    require("../assets/php/reservering.php");
+
+    
 ?>
 
 <html>
     <style>
+        body {
+            background-color: #f5f5f5;
+        }
 
         #reserveringen-container{
-            background-color: rgba(0,0,0,0.04);
+            background-color: #ffffff;
             height: 70%;
             width: 55%;
-            margin-left: 25%; 
+            margin: 0 auto;
         }
 
     </style>
@@ -20,12 +26,23 @@
         <div id="reserveringen-container">
             <div id="reserveringen-container__options">
                 <div id="reserveringen-container__options__content">
+
                     <input type="text">
                 </div>
             </div>
             <div id="reserveringen-container__reserveringen-table">
                 <table>
                     <tr><th>Plaatsnummer</th><th>Voornaam</th><th>Tussenvoegsel</th><th>Achternaam</th><th>Begin Datum</th><th>Eind Datum</th><th>Eindbedrag</th><th>Acties</th></tr>
+                    <?php
+                        foreach(returnReserveringen() as $reservering){
+                            echo "<tr>";
+                            foreach($reservering->return_small_result() as $result){
+                                echo "<td>".$result."</td>";
+                            }
+                            echo "</tr>";
+                        }
+
+                    ?>
                 </table>
             </div>
         </div>
